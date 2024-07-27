c5c83990e97e37149dbc205bee1819f3e66c16de
Brian Brewer <bbrewer.email@gmail.com>
2024-07-27 10:16:37 +0200

    port to cpp

--- include/isomorphism.h
+++ include/isomorphism.h
@@ -1,9 +1,6 @@
 #pragma once
 
-extern "C" {
-    #include "../src/hand_index.h"
-}
-
+#include "../src/hand_index.h"
 #include <vector>
 
 //imperfect recall is HandIsomorphism({2},{2,3},{2,4},{2,5});
@@ -14,24 +11,25 @@ public:
     HandIsomorphism(std::vector<std::vector<uint8_t>> cardsPerRound):cardsPerRound(cardsPerRound){
         indexers.resize(cardsPerRound.size());
         for (size_t i = 0; i < indexers.size(); i++){
-            hand_indexer_init(cardsPerRound[i].size(), cardsPerRound[i].data(), &indexers[i]);
+            indexInfo.hand_indexer_init(cardsPerRound[i].size(), cardsPerRound[i].data(), &indexers[i]);
         }
     }
     ~HandIsomorphism(){
         for (size_t i = 0; i < indexers.size(); i++){
-            hand_indexer_free(&indexers[i]);
+            indexInfo.hand_indexer_free(&indexers[i]);
         }
     }
-    uint64_t size(int round) const {
-        return hand_indexer_size(&indexers[round], cardsPerRound[round].size() - 1);
+    uint64_t size(int round) {
+        return indexInfo.hand_indexer_size(&indexers[round], cardsPerRound[round].size() - 1);
     }
-    uint64_t index(int round, uint8_t const* cards) const {
-        return hand_index_last(&indexers[round], cards);
+    uint64_t index(int round, uint8_t const* cards) {
+        return indexInfo.hand_index_last(&indexers[round], cards);
     }
-    bool unindex(int round, uint64_t index, uint8_t* output) const{
-        return hand_unindex(&indexers[round], cardsPerRound[round].size() - 1, index, output);
+    bool unindex(int round, uint64_t index, uint8_t* output){
+        return indexInfo.hand_unindex(&indexers[round], cardsPerRound[round].size() - 1, index, output);
     }
 private:
+    HandIndexer indexInfo;
     std::vector<std::vector<uint8_t>> cardsPerRound;
     std::vector<hand_indexer_t> indexers;
 };
--- src/hand_index.h
+++ src/hand_index.h
@@ -11,9 +11,8 @@
 #ifndef _HAND_INDEX_H_
 #define _HAND_INDEX_H_
 
-#include <inttypes.h>
-#include <stddef.h>
-#include "deck.h"
+#include <cstdint>
+#include <functional>
 
 #define MAX_ROUNDS           8
 
@@ -23,78 +22,50 @@ typedef struct hand_indexer_state_s hand_indexer_state_t;
 
 #define PRIhand_index        PRIu64
 
-/**
- * Initialize a hand indexer.  This generates a number of lookup tables and is relatively
- * expensive compared to indexing a hand.
- *
- * @param rounds number of rounds
- * @param cards_per_round number of cards in each round
- * @param indexer 
- */
-bool hand_indexer_init(uint_fast32_t rounds, const uint8_t cards_per_round[], hand_indexer_t * indexer);
+#define MAX_GROUP_INDEX        0x100000 
+#define MAX_CARDS_PER_ROUND    15
+#define ROUND_SHIFT            4
+#define ROUND_MASK             0xf
+#define SUITS     4
+#define RANKS    13
+#define CARDS    52
 
-/**
- * Free a hand indexer.
- *
- * @param indexer
- */
-void hand_indexer_free(hand_indexer_t * indexer);
+class HandIndexer {
+public:
+    HandIndexer();
+    ~HandIndexer();
+    bool hand_indexer_init(uint_fast32_t rounds, const uint8_t cards_per_round[], hand_indexer_t * indexer);
+    void hand_indexer_free(hand_indexer_t * indexer);
+    hand_index_t hand_indexer_size(const hand_indexer_t * indexer, uint_fast32_t round);
+    hand_index_t hand_index_all(const hand_indexer_t * indexer, const uint8_t cards[], hand_index_t indices[]);
+    hand_index_t hand_index_last(const hand_indexer_t * indexer, const uint8_t cards[]);
+    hand_index_t hand_index_next_round(const hand_indexer_t * indexer, const uint8_t cards[], hand_indexer_state_t * state);
+    bool hand_unindex(const hand_indexer_t * indexer, uint_fast32_t round, hand_index_t index, uint8_t cards[]);
 
-/**
- * @param indexer
- * @param round 
- * @returns size of index for hands on round
- */
-hand_index_t hand_indexer_size(const hand_indexer_t * indexer, uint_fast32_t round);
+private:
+    uint8_t (*nth_unset)[RANKS];
+    bool (*equal)[SUITS];
+    uint_fast32_t (*nCr_ranks)[RANKS + 1];
+    uint_fast32_t *rank_set_to_index;
+    uint_fast32_t (*index_to_rank_set)[1 << RANKS];
+    uint_fast32_t (*suit_permutations)[SUITS];
+    hand_index_t (*nCr_groups)[SUITS + 1];
 
-/**
- * Initialize a hand index state.  This is used for incrementally indexing a hand as
- * new rounds are dealt and determining if a hand is canonical.
- *
- * @param indexer
- * @param state
- */
-void hand_indexer_state_init(const hand_indexer_t * indexer, hand_indexer_state_t * state);
+    void hand_index_ctor();
+    void hand_index_dtor();
 
-/**
- * Index a hand on every round.  This is not more expensive than just indexing the last round.
- *
- * @param indexer
- * @param cards
- * @param indices
- * @returns hand's index on the last round
- */
-hand_index_t hand_index_all(const hand_indexer_t * indexer, const uint8_t cards[], hand_index_t indices[]);
+    void enumerate_configurations_r(uint_fast32_t rounds, const uint8_t cards_per_round[], uint_fast32_t round, uint_fast32_t remaining, uint_fast32_t suit, uint_fast32_t equal, uint_fast32_t used[], uint_fast32_t configuration[], std::function<void(uint_fast32_t, uint_fast32_t*, void*)> observe, void * data);
+    void enumerate_configurations(uint_fast32_t rounds, const uint8_t *cards_per_round, std::function<void(uint_fast32_t, uint_fast32_t*, void*)> observe, void* data);
+    void count_configurations(uint_fast32_t round, uint_fast32_t configuration[], void * data);
+    void tabulate_configurations(uint_fast32_t round, uint_fast32_t configuration[], void * data);
 
-/**
- * Index a hand on the last round.
- *
- * @param indexer
- * @param cards
- * @returns hand's index on the last round
- */
-hand_index_t hand_index_last(const hand_indexer_t * indexer, const uint8_t cards[]);
+    void enumerate_permutations_r(uint_fast32_t rounds, const uint8_t cards_per_round[], uint_fast32_t round, uint_fast32_t remaining, uint_fast32_t suit, uint_fast32_t used[], uint_fast32_t count[], std::function<void(uint_fast32_t, uint_fast32_t*, void*)> observe, void * data);
+    void enumerate_permutations(uint_fast32_t rounds, const uint8_t cards_per_round[], std::function<void(uint_fast32_t, uint_fast32_t*, void*)> observe, void * data);
+    void count_permutations(uint_fast32_t round, uint_fast32_t count[], void * data);
+    void tabulate_permutations(uint_fast32_t round, uint_fast32_t count[], void * data);
 
-/**
- * Incrementally index the next round.
- * 
- * @param indexer
- * @param cards the cards for the next round only!
- * @param state
- * @returns the hand's index at the latest round
- */
-hand_index_t hand_index_next_round(const hand_indexer_t * indexer, const uint8_t cards[], hand_indexer_state_t * state);
-
-/**
- * Recover the canonical hand from a particular index.
- *
- * @param indexer
- * @param round
- * @param index
- * @param cards
- * @returns true if successful
- */
-bool hand_unindex(const hand_indexer_t * indexer, uint_fast32_t round, hand_index_t index, uint8_t cards[]);
+    void hand_indexer_state_init(const hand_indexer_t * indexer, hand_indexer_state_t * state);
+};
 
 #include "hand_index-impl.h"
 
