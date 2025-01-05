2aa79560bef37f82f09761e9ad9cf44b20bf7540
Brian Brewer <44099384+brewer-b@users.noreply.github.com>
2025-01-05 08:55:25 +0100

    Change _Bool to bool

--- src/hand_index-impl.h
+++ src/hand_index-impl.h
@@ -1,6 +1,6 @@
 /**
  * hand_index-impl.h
- * 
+ *
  * @author Kevin Waugh (waugh@cs.cmu.edu)
  * @date April 13, 2013
  */
@@ -13,10 +13,11 @@ struct hand_indexer_s {
   uint_fast32_t rounds, configurations[MAX_ROUNDS], permutations[MAX_ROUNDS];
   hand_index_t round_size[MAX_ROUNDS];
 
-  uint_fast32_t * permutation_to_configuration[MAX_ROUNDS], * permutation_to_pi[MAX_ROUNDS], * configuration_to_equal[MAX_ROUNDS];  
-  uint_fast32_t (* configuration[MAX_ROUNDS])[SUITS];
-  uint_fast32_t (* configuration_to_suit_size[MAX_ROUNDS])[SUITS];
-  hand_index_t * configuration_to_offset[MAX_ROUNDS];
+  uint_fast32_t *permutation_to_configuration[MAX_ROUNDS],
+      *permutation_to_pi[MAX_ROUNDS], *configuration_to_equal[MAX_ROUNDS];
+  uint_fast32_t (*configuration[MAX_ROUNDS])[SUITS];
+  uint_fast32_t (*configuration_to_suit_size[MAX_ROUNDS])[SUITS];
+  hand_index_t *configuration_to_offset[MAX_ROUNDS];
 };
 
 struct hand_indexer_state_s {
